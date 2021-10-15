90654d311ff78d49889289d437867d4a5907da40
Amie Foster <amie.n.foster@gmail.com>
2021-10-15 11:59:58 -0600

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr. Chocolate", 6, "M", "brown", "friendly", 1),
+("Rowdy", 7, "F", "white", "friendly", 1),
+("Tabitha", 21, "F", "black", "at peace", 0),
+("Sergeant Brown", 41, "M", "bown", "angry", 1),
+("Melissa", 2, "F", "black", "curious", 1),
+("Grinch", 15, "M", "white", "angry", 1),
+("Wendy", 62, "F", "bown", "quiet", 0),
+(NULL, 10, "M", "brown", "loud", 1);
